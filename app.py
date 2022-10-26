1439592a3c51b3c88380153058fe443bae0c605b
Mrunank Pawar <mrunankpawar05@gmail.com>
2022-10-26 11:38:55 +0530

    added strings in python

--- app.py
+++ app.py
@@ -5,15 +5,27 @@
 
 # //TODO: Variables
 
-name = "Mrunank"
-age = 21
+# name = "Mrunank"
+# age = 21
 
 # print(name)
 # print(name+ " is a boy")
-print(name, "is", age)
+# print(name, "is", age)
 # print(name+ " lives in Pune")
 
+# //TODO: Strings in Python
 
+# name = 'MRUNANK'
 
+# print("Hi.\nHow are you?")
 
+# print(name.upper())
+# print(name.lower())
+# print(name.isupper())
+# print(name.islower())
+# print(name.lower().islower()) 
+# print(len(name))
+# print(name.index('A'))
+# print(name.replace("M", "K"))
 
+# //TODO: Numbers in Python
