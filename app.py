a0be0d22b32c6ccdd34c74063dc8deff33669d91
Mrunank Pawar <mrunankpawar05@gmail.com>
2022-10-26 10:02:18 +0530

    added app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+#  //TODO: Print 
+
+# print("Hello World")
+# print("Welcome")
+
+# //TODO: Variables
+
+name = "Mrunank"
+age = 21
+
+# print(name)
+# print(name+ " is a boy")
+print(name, "is", age)
+# print(name+ " lives in Pune")
+
+
+
+
+
