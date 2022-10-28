9ac71c9c09ebab4545c28b857edb53450df757da
Mrunank Pawar <mrunankpawar05@gmail.com>
2022-10-28 18:26:38 +0530

    added taking input

--- app.py
+++ app.py
@@ -30,23 +30,35 @@
 
 # //TODO: Numbers in Python
 
-from math import *
+# from math import *
 
-number = 20
-number2 = str(number)
-print(78)
-print(78 + 54)
-print(78 - 54.34523)
-print(20 / 6)
-print(20 % 6)
-print("The number is : " + number2)
-print(abs(-5))
-print(max(10, 6, -12, 15))
-print(min(10, 6, -12, 15))
-print(round(3.6))
-print(round(3.2)) 
-print(bin(5401))
-print(sqrt(100))
+# number = 20
+# number2 = str(number)
+# print(78)
+# print(78 + 54)
+# print(78 - 54.34523)
+# print(20 / 6)
+# print(20 % 6)
+# print("The number is : " + number2)
+# print(abs(-5))
+# print(max(10, 6, -12, 15))
+# print(min(10, 6, -12, 15))
+# print(round(3.6))
+# print(round(3.2)) 
+# print(bin(5401))
+# print(sqrt(100))
 
+# //TODO: User input
 
+# name = input("What's your name: ")
+# age = input("What's your age: ")
+# print("My name is", name, "and I'm", age, "years old")
+
+# //TODO: Replace word in a sentence
+
+# sentence = input("Enter your sentence: ")
+# print('Your sentence is: ', sentence)
+# word1 = input("Enter the word to replace: ")
+# word2 = input("Enter the word to replace it with: ")
+# print(sentence.replace(word1, word2))
 
